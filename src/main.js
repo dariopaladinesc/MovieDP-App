9796ddcd23bd73837af23086d3f7f91d799362a2
Dario Paladines <dariopaladines.23@gmail.com>
2022-06-08 10:43:17 -0500

    primer consumo de la API, cargando peliculas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log("hola mundo")
+const url_api =  "ebef3c4904b620dd2750ccb92c78cdc6"
+
+async function getTrendingPreview(){
+    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + url_api)
+    const data = await res.json();
+
+    const movies = data.results;
+    console.log({ data, movies});
+    movies.forEach(movie => {
+        const trendingPreview = document.querySelector("#trend-section-container .trend-container-figures")
+
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("figure-container--movie-list");
+        
+
+        const movieImg = document.createElement("img");
+        movieImg.classList.add("imgMovie");
+        movieImg.setAttribute("alt", movie.title);
+        movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w300" + movie.poster_path);
+        
+        movieContainer.appendChild(movieImg);
+        trendingPreview.appendChild(movieContainer)
+    })
+    
+}
+
+getTrendingPreview();
