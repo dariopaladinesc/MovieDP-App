9ba503096d393c69fe5828371ac551ee65234d65
Dario Paladines <dariopaladines.23@gmail.com>
2022-06-08 11:49:49 -0500

    agregando el titulo y puntuacion de c/pelicula en el consumo de la API

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+///NOTAS 
+////MANIPULACION DEL DOM --> Al manipular el dom tenemos que tener la misma estructura del html (clases, id, etiquetas) para que se enlazen con css y js
+////Los appendchild nos sirven para crear nodos donde el primer valor es el contenedor padre y el que va en parentesis el contenedor o etq hija (tener en cuenta que para que funcione, siempre se deben tener la misma estructura del HTML, irse guiando por el nombre de las clases)
+
+
+
+
 const url_api =  "ebef3c4904b620dd2750ccb92c78cdc6"
 
 async function getTrendingPreview(){
@@ -7,19 +14,35 @@ async function getTrendingPreview(){
     const movies = data.results;
     console.log({ data, movies});
     movies.forEach(movie => {
+        //DOM (Buscar Notas)
         const trendingPreview = document.querySelector("#trend-section-container .trend-container-figures")
 
-        const movieContainer = document.createElement("div");
-        movieContainer.classList.add("figure-container--movie-list");
+        const movieContainer = document.createElement("div"); //Esto nos permite crear una nueva etiqueta div
+        movieContainer.classList.add("figure-container--movie-list"); //le agregamos la clase de esta misma etiqueta, misma clase que esta en el html
         
-
-        const movieImg = document.createElement("img");
+        const movieImg = document.createElement("img"); 
         movieImg.classList.add("imgMovie");
-        movieImg.setAttribute("alt", movie.title);
+        movieImg.setAttribute("alt", movie.title); //Creamos los atributos de la etq img, donde pasan dos valores, el tipo de atributo y el valor de dicho atributo 
         movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w300" + movie.poster_path);
         
-        movieContainer.appendChild(movieImg);
-        trendingPreview.appendChild(movieContainer)
+        const titleMovieContainer = document.createElement("div"); //etq contenedor del titulo de la movie y la calificacion
+        titleMovieContainer.classList.add("description-movie");
+        const titleMovie = document.createElement("p");
+        titleMovie.classList.add("titleOfMovie");
+        const textP = document.createTextNode(movie.original_title) //estamos añadiendo un texto a la etiqueta p
+
+        const rateMovie = document.createElement("p");
+        rateMovie.classList.add("rate");
+        const textRate = document.createTextNode("Rate:  " + movie.vote_average +"pts")
+
+        //APENDCHILD (buscar en notas)
+        movieContainer.appendChild(movieImg); //movieContainer es una etq div padre que contiene la etq img
+        trendingPreview.appendChild(movieContainer); //TrendigPreview es la etiqueta contenedor padre de todos los elementos
+        titleMovie.appendChild(textP);//Estamos agregando el texto a la etq p
+        titleMovieContainer.appendChild(titleMovie, rateMovie); //la etq p(titleMovie) es hija del contenedor div
+        rateMovie.appendChild(textRate);
+        titleMovieContainer.appendChild(rateMovie);//Mismo procedimiento que para titleMovie
+        movieContainer.appendChild(titleMovieContainer); //finalmente agregamos toda la estructura del titulo de la pelicula y puntuacion al contenedor padre div
     })
     
 }
