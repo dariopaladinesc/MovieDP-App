00817521479c68db4015646f9e02a4984fb51b71
Dario Paladines <dariopaladines.23@gmail.com>
2022-06-11 13:32:29 -0500

    creando el archivo nodes con la primera version de nodos para Js

---
+++ src/node.js
@@ -0,0 +1,39 @@
+
+
+
+/*    HEADER SECTION   */
+
+const principalheader = document.querySelector(".principal-header");
+const principalnav = document.querySelector(".principal-nav");
+const navLogoContainer = document.querySelector(".principal-logo--container");
+const navSectionMovies = document.querySelector(".nav-section-movies");
+const navSearch = document.querySelector(".nav-search");
+
+
+/*   MOVIE DESCRIPTION  */
+
+const movieSection = document.querySelector(".movieSection");
+const movieSection_movieContainer = document.querySelector(".movieSection-MoviesContainer");
+const titleMovieContainer = document.querySelector(".movieSection-MoviesContainer--titleMovie");
+const movieDescription = document.querySelector(".categories-section__moviedescription");
+const listCategories = document.querySelector(".categories-section--listMovie");
+const titleSimilars = document.querySelector(".similares");
+const similarSection = document.querySelector(".similarSection");
+const similarMovieList = document.querySelector(".figure-container--movie-list");
+
+
+/*   HOME SECTION  */
+
+const homeSection = document.querySelector(".homeSection");
+const mainSection = document.querySelector(".principal-main-section");
+const buttonMainContainer = document.querySelector(".main-section--buttons-container");
+const trendSection = document.querySelector(".trendSection");
+const titleTrendSection = document.querySelector(".title-section");
+const trendContainerImg = document.querySelector(".trend-container-figures");
+const viewSection = document.querySelector(".viewSection");
+const viewSectionContainer = document.querySelector(".trend-section-container");
+const viewContainerFigures = document.querySelector(".trend-container-figures");
+const categoriesSectionView = document.querySelector(".categories-section");
+
+
+
